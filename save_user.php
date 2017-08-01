5a736e6f1be6175737d1b4ab09a46e2d95d72502
passionate0922 <njw392@gmail.com>
2017-08-01 11:33:02 -0500

    Add files via upload

---
+++ save_user.php
@@ -0,0 +1,25 @@
+﻿<?php
+ $host = 'localhost';
+ $user = 'SAMSUNG\USER';
+ $dbName = 'users_list';
+ $mysqli = new mysqli($host, $user, $dbName);
+ 
+ function id_inc(){
+ 
+ };
+ $id = 0;
+ $firstname = $_POST['FName'];
+ $lastname = $_POST['LName'];
+ $password = md5($_POST['PWD']);	//md5, for security
+ $email=$_POST['EMAIL'];
+ 
+ $sql = "INSERT INTO userlist (id, firstname, lastname, pwd, email)";
+ $sql = $sql. "values('$id','$firstname','$lastname','$password','$email')";
+ $id++;
+
+ if($mysqli->query($sql)){
+  echo 'success';	//For testing
+ }else{
+  echo 'fail';
+ }
+?>
